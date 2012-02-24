2b69a3c3de2583cc8b22f6e95c0f4664e5c6af7e
Florian Pfeiffer <florian.pfeiffer.git@x8s.de>
2012-02-24 10:33:53 +0100

    updated packages urls to Percona XtraDB Cluster 5.5.20-beta23.4

--- cookbooks/xtradbcluster/recipes/fetchPackages.rb
+++ cookbooks/xtradbcluster/recipes/fetchPackages.rb
@@ -1,16 +1,16 @@
 
 
-base = 'http://www.percona.com/downloads/Percona-XtraDB-Cluster/5.5.17-alpha22.1/deb/squeeze/'
+base = 'http://www.percona.com/downloads/Percona-XtraDB-Cluster/5.5.20-beta23.4/deb/squeeze/x86_64/'
 
 dlFiles = [
-  "#{base}libmysqlclient18_5.5.17-22.1-3691.squeeze_amd64.deb",
-  "#{base}libmysqlclient-dev_5.5.17-22.1-3691.squeeze_amd64.deb",
-  "#{base}percona-xtradb-cluster-common-5.5_5.5.17-22.1-3691.squeeze_all.deb",
-  "#{base}percona-xtradb-cluster-client-5.5_5.5.17-22.1-3691.squeeze_amd64.deb",
+  "#{base}libmysqlclient18_5.5.20-beta23.4-3724.squeeze_amd64.deb",
+  "#{base}libmysqlclient-dev_5.5.20-beta23.4-3724.squeeze_amd64.deb",
+  "#{base}percona-xtradb-cluster-common-5.5_5.5.20-beta23.4-3724.squeeze_all.deb",
+  "#{base}percona-xtradb-cluster-client-5.5_5.5.20-beta23.4-3724.squeeze_amd64.deb",
   "http://www.percona.com/downloads/XtraBackup/BETA/1.9.0/deb/squeeze/xtrabackup_1.9.0-385.squeeze_amd64.deb",
   "#{base}percona-xtradb-cluster-galera_0.1-1_amd64.deb",
-  "#{base}percona-xtradb-cluster-server-5.5_5.5.17-22.1-3691.squeeze_amd64.deb",
-  "#{base}percona-xtradb-cluster-test-5.5_5.5.17-22.1-3691.squeeze_all.deb",
+  "#{base}percona-xtradb-cluster-server-5.5_5.5.20-beta23.4-3724.squeeze_amd64.deb",
+  "#{base}percona-xtradb-cluster-test-5.5_5.5.20-beta23.4-3724.squeeze_all.deb",
 ]
 
 
